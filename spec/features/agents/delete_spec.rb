e6d204ee6677cee7d7c248a07c973beaa1410411
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-13 18:26:35 -0400

    Complete user story 22. Add test to agent delete_spec and modify agent index to add delete agent link

--- spec/features/agents/delete_spec.rb
+++ spec/features/agents/delete_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe 'destroying an agent', type: :feature do
     expect(current_path).to eq("/agents")
     expect(page).to_not have_content('Sally Closer')
   end
+
+  it 'can delete the agent from the index page' do
+    agent = Agent.create!( name:             "Sally Closer",
+                           licensed_realtor: true,
+                           review_rating:    4.5)
+    house_1 = agent.houses.create!( address:     "292 5th Ave",
+                                    price:       195000,
+                                    for_sale:    true)
+    house_2 = agent.houses.create!( address:     "12 Circle Dr",
+                                    price:       105000,
+                                    for_sale:    false)
+    visit "/agents"
+
+    click_link('Delete Agent')
+    expect(current_path).to eq("/agents")
+    expect(page).to_not have_content('Sally Closer')
+  end
 end
